b423b02aabf2be4b4e5060219c64e364370f2009
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-04-13 08:16:22 +0100

    project setup/clean up complete

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import '../styles/app.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
