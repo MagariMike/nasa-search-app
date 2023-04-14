a5c4605d7df1bf8709ef6190b291c1c1aea7be6a
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-04-14 08:24:25 +0100

    logo added and initial seach component added, inc basic tests for both

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import "../styles/search.css";
+
+const Search = () => {
+    return (
+        <>
+        <input className="search-input" type="text"></input>
+        </>
+    );
+};
+
+export default Search;
