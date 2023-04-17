92cc514e0c95ee9f93443f1af83139932c8944c8
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-04-17 08:46:03 +0100

    getImages.js complete

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@
 import React from "react";
 import '../styles/app.css';
 import Search from "../components/Search";
+
 const App = () => {
   
   return (
     <div className="App">
-      <img cassName="nasa-logo" src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" alt="NASA Logo"></img>
+      <img className="nasa-logo" src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" alt="NASA Logo"></img>
       <Search />
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { useState } from "react";
 import "../styles/search.css";
+import getImages from "../requests/getImages";
 
 const Search = () => {
+    const [value, setValue] = useState();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        getImages(value);
+    }
     return (
         <>
-        <input className="search-input" type="text"></input>
+        <form className="search-form" onSubmit={handleSubmit}>
+        <input 
+        className="search-input" 
+        type="text"
+        onChange={(e) => setValue(e.target.value)}></input>
+        <button className="search-btn" 
+        type="submit">Go!</button>
+        </form>
         </>
     );
 };
