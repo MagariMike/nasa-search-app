ca95281ddffac15ed5a3e0e124136b607213bbaa
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-04-17 20:06:41 +0100

    structure refactoring complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 
-import React from "react";
+import React, { useState } from "react";
 import '../styles/app.css';
 import Search from "../components/Search";
 
 const App = () => {
-  
+  const [searchResults, setSearchResults] = useState();
   return (
     <div className="App">
       <img className="nasa-logo" src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" alt="NASA Logo"></img>
-      <Search />
+      <Search setSearchResults={setSearchResults} />
     </div>
   );
 }
